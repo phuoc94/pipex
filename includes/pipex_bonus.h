65357883d0c18a6aa91a6b6dcdc21845e3522f48
Phuoc Nguyen <phuoc94@hotmail.fi>
2025-01-12 19:10:10 +0200

    Refactor: main_bonus.c and format header

--- includes/pipex_bonus.h
+++ includes/pipex_bonus.h
@@ -6,34 +6,34 @@
 /*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 12:56:43 by phuocngu          #+#    #+#             */
-/*   Updated: 2025/01/12 14:57:16 by phuocngu         ###   ########.fr       */
+/*   Updated: 2025/01/12 19:07:37 by phuocngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
 
 # include "../lib/libft/includes/libft.h"
 # include <sys/wait.h> // waitpid
 
 typedef struct s_pipex
 {
-	int argc;
-	char **argv;
-	char **envp;
-	int prev_fd;
-	int fd[2];
-}	t_pipex;
+	int		argc;
+	char	**argv;
+	char	**envp;
+	int		prev_fd;
+	int		fd[2];
+}			t_pipex;
 
-int		validate_args(int argc, char **argv);
-int		create_pipe(int fd[2]);
-int		create_fork(void);
-void	execute_command(char *cmd, char **envp);
-void	handle_first_child(t_pipex *pipex);
-void	handle_middle_child(t_pipex *pipex);
-void	handle_last_child(t_pipex *pipex);
-char	*find_cmd_path(char *cmd, char **envp);
+int			validate_args(int argc, char **argv);
+int			create_pipe(int fd[2]);
+int			create_fork(void);
+void		execute_command(char *cmd, char **envp);
+void		handle_first_child(t_pipex *pipex);
+void		handle_middle_child(t_pipex *pipex);
+void		handle_last_child(t_pipex *pipex);
+char		*find_cmd_path(char *cmd, char **envp);
 
-void	ft_perror(char *message, char *detail);
-int		close_pipe(int fd[2]);
+void		ft_perror(char *message, char *detail);
+int			close_pipe(int fd[2]);
 #endif
--- src/bonus/main_bonus.c
+++ src/bonus/main_bonus.c
@@ -6,44 +6,44 @@
 /*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/05 07:26:04 by phuocngu          #+#    #+#             */
-/*   Updated: 2025/01/12 18:55:11 by phuocngu         ###   ########.fr       */
+/*   Updated: 2025/01/12 19:09:56 by phuocngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/pipex_bonus.h"
 
-int	main(int argc, char **argv, char **envp)
+void	execute_pipes(t_pipex *pipex)
 {
-	int		pid;
-	int		status;
-	int		i;
-	t_pipex	pipex;
+	int	pid;
+	int	i;
 
-	validate_args(argc, argv);
-	pipex.argc = argc;
-	pipex.argv = argv;
-	pipex.envp = envp;
-	pipex.prev_fd = -1;
 	i = 0;
-	while (i < argc - 3)
+	while (i < pipex->argc - 3)
 	{
-		create_pipe(pipex.fd);
+		create_pipe(pipex->fd);
 		pid = create_fork();
 		if (pid == 0)
 		{
 			if (i == 0)
-				handle_first_child(&pipex);
-			else if (i == argc - 4)
-				handle_last_child(&pipex);
+				handle_first_child(pipex);
+			else if (i == pipex->argc - 4)
+				handle_last_child(pipex);
 			else
-				handle_middle_child(&pipex);
+				handle_middle_child(pipex);
 		}
-		close(pipex.prev_fd);
-		pipex.prev_fd = pipex.fd[0];
-		close(pipex.fd[1]);
+		close(pipex->prev_fd);
+		pipex->prev_fd = pipex->fd[0];
+		close(pipex->fd[1]);
 		i++;
 	}
-	close(pipex.prev_fd);
+	close(pipex->prev_fd);
+}
+
+int	wait_for_children(int argc)
+{
+	int	status;
+	int	i;
+
 	i = 0;
 	while (i < argc - 3)
 	{
@@ -55,4 +55,17 @@ int	main(int argc, char **argv, char **envp)
 		i++;
 	}
 	return (status >> 8 & 255);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_pipex	pipex;
+
+	validate_args(argc, argv);
+	pipex.argc = argc;
+	pipex.argv = argv;
+	pipex.envp = envp;
+	pipex.prev_fd = -1;
+	execute_pipes(&pipex);
+	return (wait_for_children(argc));
 }
