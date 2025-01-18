5b79e90fe1804683455f6cea35bf1f9a33ff2c30
Phuoc Nguyen <phuoc94@hotmail.fi>
2025-01-18 13:09:26 +0200

    Refactor get_next_line: use libft functions

--- lib/libft/includes/get_next_line.h
+++ lib/libft/includes/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 16:47:12 by phuocngu          #+#    #+#             */
-/*   Updated: 2025/01/06 13:44:43 by phuocngu         ###   ########.fr       */
+/*   Updated: 2025/01/18 13:05:45 by phuocngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,36 +17,10 @@
 #  define BUFFER_SIZE 12
 # endif
 
-# include <fcntl.h> //for open file
-# include <stdlib.h> // for malloc
-# include <unistd.h> // read
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft.h"
 
-typedef struct s_mynode
-{
-	char			*content;
-	struct s_mynode	*next;
-}					t_mynode;
-
-typedef struct s_list
-{
-	t_mynode		*head;
-	t_mynode		*tail;
-}					t_list;
-
-typedef struct s_line_data
-{
-	char	*current_line;
-	int		current_line_len;
-	char	*next_line;
-}				t_line_data;
-
-char				*get_next_line(int fd);
-void				add_to_list(t_list *list, char *buffer);
-void				create_list(t_list **list, int fd);
-int					found_newline(t_mynode *node);
-void				free_list(t_list *list);
-size_t				count_line_len(t_list *list);
-t_list				*reinitialize_list(t_list *list, char *next_line_head);
-char				*copy_list_to_line(t_list *list, char *next_line_head);
+char	*get_next_line(int fd);
 
 #endif
