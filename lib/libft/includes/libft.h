e85725b34839d290eb62f748fc71cdcf184b9303
Phuoc Nguyen <phuoc94@hotmail.fi>
2025-01-21 12:07:56 +0200

    Add ft_strallspace function to check if a string contains only whitespace

--- lib/libft/includes/libft.h
+++ lib/libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: phuocngu <phuocngu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 14:53:10 by phuocngu          #+#    #+#             */
-/*   Updated: 2025/01/11 21:48:48 by phuocngu         ###   ########.fr       */
+/*   Updated: 2025/01/21 12:03:07 by phuocngu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 void	free_ft_split(char ***ptr_strs);
+int		ft_strallspace(const char *s);
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
